19d6986bf385fa35a5e8cd713e9fc6d463f93d44
alonzoDiego <dhilariocallupe@gmail.com>
2021-08-22 00:01:08 -0500

    Home nutricionista

---
+++ lib/models/Paciente.dart
@@ -0,0 +1,52 @@
+class Paciente{
+  String name;
+  String photo;
+  String diagnosis;
+  double progress;
+
+  Paciente({
+    required this.name,
+    required this.photo,
+    required this.diagnosis,
+    required this.progress
+  });
+}
+
+List<Paciente> pacientes = [
+  Paciente(
+    name: "Rodrigo Morales",
+    photo: "https://static-cdn.jtvnw.net/jtv_user_pictures/986abe5c-d7ee-4f2b-818f-a5a421e481f2-profile_image-300x300.png",
+    diagnosis: "Paciente diabético",
+    progress: 0.70
+  ),
+  Paciente(
+    name: "Rodrigo Morales",
+    photo: "https://static-cdn.jtvnw.net/jtv_user_pictures/986abe5c-d7ee-4f2b-818f-a5a421e481f2-profile_image-300x300.png",
+    diagnosis: "Paciente diabético",
+    progress: 0.70
+  ),
+  Paciente(
+    name: "Rodrigo Morales",
+    photo: "https://static-cdn.jtvnw.net/jtv_user_pictures/986abe5c-d7ee-4f2b-818f-a5a421e481f2-profile_image-300x300.png",
+    diagnosis: "Paciente diabético",
+    progress: 0.70
+  ),
+  Paciente(
+    name: "Rodrigo Morales",
+    photo: "https://static-cdn.jtvnw.net/jtv_user_pictures/986abe5c-d7ee-4f2b-818f-a5a421e481f2-profile_image-300x300.png",
+    diagnosis: "Paciente diabético",
+    progress: 0.70
+  ),
+  Paciente(
+    name: "Rodrigo Morales",
+    photo: "https://static-cdn.jtvnw.net/jtv_user_pictures/986abe5c-d7ee-4f2b-818f-a5a421e481f2-profile_image-300x300.png",
+    diagnosis: "Paciente diabético",
+    progress: 0.70
+  ),
+  Paciente(
+    name: "Rodrigo Morales",
+    photo: "https://static-cdn.jtvnw.net/jtv_user_pictures/986abe5c-d7ee-4f2b-818f-a5a421e481f2-profile_image-300x300.png",
+    diagnosis: "Paciente diabético",
+    progress: 0.70
+  )
+];
